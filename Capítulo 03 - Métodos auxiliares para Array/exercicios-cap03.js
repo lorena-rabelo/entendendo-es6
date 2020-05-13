a0d4e958492e84473a535fb6109edf57cb988dec
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-05-13 00:26:38 -0300

    Resolução exercícios 2 e 3 - cap. 3

--- Capítulo 03 - Métodos auxiliares para Array/exercicios-cap03.js
+++ Capítulo 03 - Métodos auxiliares para Array/exercicios-cap03.js
@@ -1,5 +1,5 @@
 // Exercício 1 - Par ou ímpar?
-// Altere o código a seguir para utilizar o método `forEach` de modo que a saída permaneça a mesma.7
+// Altere o código a seguir para utilizar o método `forEach` de modo que a saída permaneça a mesma.
 
 // var numeros = [0,1,2,3,4,5];
 // for(var i = 0; i<= numeros.length; i++) {
@@ -18,10 +18,8 @@
 // 4 é par
 // 5 é ímpar
 
-
-
-
 var numeros = [0, 1, 2, 3, 4, 5];
+
 numeros.forEach(function (numero) {
   if (numero % 2 === 0) {
     console.log(numero + ' é par');
@@ -30,3 +28,26 @@ numeros.forEach(function (numero) {
   }
 })
 
+
+
+// Exercício 2 - Quero o dobro
+// Utilizando o método `map`, escreva o método `dobrar` que recebe um array de números inteiros e retorna um array com todos os valores do array original dobrados.
+
+let numeros = [1,2,3];
+let dobro = numeros.map(function(numero){
+  return numero * 2;
+});
+console.log(dobro);
+
+
+// Exercício 3 - NÃO ESTOU BRAVO
+// Escreva o método `caps` que recebe um array de strings e retorna um outro array com todas as strings do array original em CAIXA ALTA.
+
+// * Exemplo: caps(['oi', 'tudo', 'bem?']) → ['OI', 'TUDO', 'BEM?']
+
+let caixaBaixa = ['cem', 'anos', 'de', 'solidão'];
+
+let caixaAlta = caixaBaixa.map(function(item){
+  return item.toUpperCase();
+})
+console.log(caixaAlta);
